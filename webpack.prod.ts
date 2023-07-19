280b92c262a5a074eb229e42e50308e2e00e091d
Lars van Vianen <lars@vianen.com>
2023-07-19 21:51:22 +0200

    Create webpack.prod.ts

---
+++ webpack.prod.ts
@@ -0,0 +1,48 @@
+// import path from 'path'
+// import { fileURLToPath } from 'url';
+// import webpack from 'webpack';
+
+import paths from './webpack.paths';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const configProduction: any = {
+
+    mode: 'production',
+
+    devtool: 'source-map',
+
+    entry: {
+        'block.gl': [paths.entryPoint],
+        'block.gl.min': [paths.entryPoint]
+    },
+
+    resolve: {
+        extensions: [
+            '.ts', '.tsx',
+            '.js', '.jsx',
+        ],
+    },
+
+    output: {
+        library: 'block.gl',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
+        path: paths.lib,
+        filename: '[name].js',
+        umdNamedDefine: true
+    },
+
+    stats: {
+        errorDetails: true,
+        children: true
+    }
+
+    // plugins: [
+    //     new webpack.HotModuleReplacementPlugin()
+    // ]
+
+};
+
+export default configProduction
