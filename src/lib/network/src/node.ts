6b7e56cc52d14d4c33afc0d01e4e393d1e7d6602
Lars van Vianen <lars@vianen.com>
2023-07-28 12:20:05 +0200

    Update node.ts

--- src/lib/network/src/node.ts
+++ src/lib/network/src/node.ts
@@ -13,9 +13,18 @@
 // limitations under the License.
 
 
+/**
+ * This script sets up an Express server for a blockchain node. 
+ * It includes routes for various operations including viewing the blockchain, 
+ * creating transactions, mining new blocks, etc.
+ * It uses a peer-to-peer server to communicate with other nodes in the 
+ * blockchain network.
+ */
+
+
+// Import required modules and configurations
 import express from "express";
 import bodyParser from "body-parser";
-
 import * as config from "../../blockchain/src/config";
 import Blockchain from "../../blockchain/src/blockchain";
 import Block from "../../blockchain/src/block";
@@ -24,8 +33,11 @@ import Wallet from "../../blockchain/src/wallet";
 import TransactionPool from "../../blockchain/src//transaction-pool";
 import Miner from "./node_miner";
 
+
+// Set up server port
 const HTTP_PORT: string = process.env.HTTP_PORT || "3001";
 
+// Initialize app, blockchain, wallet, transaction pool, p2p server and miner
 const app = express();
 const blockchain: Blockchain = new Blockchain();
 const wallet: Wallet = new Wallet();
@@ -33,9 +45,10 @@ const tp: TransactionPool = new TransactionPool();
 const p2pServer: P2pServer = new P2pServer(blockchain, tp);
 const miner = new Miner(blockchain, tp, wallet, p2pServer);
 
+// Use json parser
 app.use(bodyParser.json());
 
-//view balance
+// Route to view balance
 app.get(
     config.ENDPOINT_GET_BALANCE,
     (
@@ -46,52 +59,58 @@ app.get(
     }
 );
 
-//view all blocks on blockchain
+// Route to view all blocks on blockchain
 app.get(config.ENDPOINT_GET_BLOCKS, (request, response) => {
     response.json({blockchain: blockchain.chain});
 });
 
-//show wallet's public key
+// Route to show wallet's public key
 app.get(config.ENDPOINT_GET_PUBLIC_KEY, (request, response) => {    
     response.json({publicKey: wallet.publicKey});
 });
 
-//view all transactions
+// Route to view all transactions
 app.get(config.ENDPOINT_GET_TRANSACTIONS, (request, response) => {
     response.json({transactions: tp.transactions});
 });
 
-//create a transaction with user's wallet and broadcast it to other nodes
+// Route to create a transaction with user's wallet and broadcast it to other nodes
 app.post(config.ENDPOINT_POST_TRANSACTIONS, (request, response) => {
     let recipient: string = request.body.recipient;
     let amount:number = request.body.amount;
-    let transaction = wallet.createOrUpdateTransaction(recipient, amount, blockchain, tp);
+    let transaction = wallet.createOrUpdateTransaction(
+        recipient,
+        amount,
+        blockchain,
+        tp
+    );
     p2pServer.broadcastTx(transaction);
     response.redirect(config.ENDPOINT_GET_TRANSACTIONS);
 });
 
-//mines new block with transaction data
+// Route to mine new block with transaction data
 app.get(config.ENDPOINT_GET_MINE_TRANSACTIONS, (request, response) => {
     const block: Block = miner.mine();
     console.log("New block added: " + block.toString());
     response.redirect(config.ENDPOINT_GET_BLOCKS);
 });
 
-//adds new block to blockchain - generic mine endpoint for mining any data
+// Route to add new block to blockchain - generic mine endpoint for mining any data
 app.post(config.ENDPOINT_POST_MINE, (request, response) => {
     const block = blockchain.addBlock(request.body.data);
     console.log("New block added: " + block.toString());
 
-    //update other nodes as soon as new block mined
+    // Update other nodes as soon as new block mined
     p2pServer.syncChains();
 
-    //show updated chain with new block
+    // Show updated chain with new block
     response.redirect(config.ENDPOINT_GET_BLOCKS);
 });
 
+// Start server
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 })
 
-
+// Start p2p server
 p2pServer.listen();
