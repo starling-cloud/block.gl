5cad21a561e60f39c9b3d7e4e3b2e28de99dd9f3
Lars van Vianen <lars@vianen.com>
2023-07-28 12:11:32 +0200

    Update blockchain_interface.ts

--- src/lib/blockchain/src/blockchain_interface.ts
+++ src/lib/blockchain/src/blockchain_interface.ts
@@ -13,21 +13,19 @@
 // limitations under the License.
 
 
-
-
 /**
- * Blockchain Interface
- * @static
- * @memberof
- * @class
- * @description
- * @param
- * @returns
+ * @interface BlockchainInterface
+ * @description This interface represents the structure of a Blockchain. It includes an array of blocks (chain) and methods to add a new block to the chain (addBlock), validate the integrity of the chain (isValidChain), and replace the current chain with a new one (replaceChain).
+ * @property {any[]} chain - The array of blocks forming the blockchain.
+ * @method addBlock - A method that accepts a data argument and returns the new block.
+ * @method isValidChain - A method that accepts an array of blocks and returns a boolean indicating whether the chain is valid.
+ * @method replaceChain - A method that accepts a new array of blocks and returns a boolean indicating whether the chain was replaced.
  * @example
  * @see
  */
-export interface BlockchainInterface {
-    chain: any;
+ export interface BlockchainInterface {
+    chain: any[];
     addBlock(arg: any): any;
-    isValidChain(arg: any): boolean;
+    isValidChain(arg: any[]): boolean;
+    replaceChain(arg: any[]): boolean;
 }
